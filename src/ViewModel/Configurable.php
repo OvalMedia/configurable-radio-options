38a61f1d42833f4ff1e61d5c07c1d802c2357fdd
Ole Hornauer <oh@oval-media.de>
2024-08-10 16:36:08 +0200

    initial commit

---
+++ src/ViewModel/Configurable.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace OM\ConfigurableRadioOptions\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Framework\View\LayoutInterface;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\Phrase;
+
+class Configurable implements ArgumentInterface
+{
+
+}
